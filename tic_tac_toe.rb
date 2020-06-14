ccd52cfd4dad38b042268d2953ab8158a50f1b21
westonludeke <westonludeke@gmail.com>
2020-06-14 09:45:32 -0500

    removed pry

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,8 +9,6 @@
 
 =end
 
-require 'pry'
-
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
